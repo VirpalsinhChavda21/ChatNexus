76967d8a76aae55f48c1846d479d5ce5e9bf7105
Virpalsinh <chavdavirpal37@gmail.com>
2023-11-13 03:44:58 +0530

    initial commit

--- app/src/main/java/com/virpalsinhchavda/chatnexus/Verification.kt
+++ app/src/main/java/com/virpalsinhchavda/chatnexus/Verification.kt
@@ -1,11 +1,29 @@
 package com.virpalsinhchavda.chatnexus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Verification : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification)
+
+        // Reference the button
+        val backArrowButton: Button = findViewById(R.id.backarrowbutton)
+
+        // Set a click listener for the button
+        backArrowButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            fun navigateToSecondActivity(view: View) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
